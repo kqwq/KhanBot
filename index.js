931d7fe023625eca6daee6b0b37d53bfa09bb8af
LeviathanCoding <>
2021-06-05 01:11:51 +0000

    1

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const { updateProgram, updateAvatar } = require('./src/post.js');
     /* Test code */
     const ID = 6463987598180352;
     const botKaid = "kaid_3687779856757538252355877";
-    
-    //let res = await post.feedback("test --001", ID)
-    //console.log(res)
-    //await updateProgram(ID, `rect(100,100,200,200);`, "Pew Nogram", 400, 400)
 
+    //await utils.getKAAS(process.env.bot_username, process.env.bot_password);
+
+    console.log("Bot Running")
 })();
-/*node_modules
-.DS_Store
-.env */
